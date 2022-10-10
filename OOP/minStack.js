946cd67635ff04966c15d1a74fcf54a578bb968c
Brian Bui <brianbui012@gmail.com>
2022-10-10 13:50:27 -0700

    Fixed min stack to return O(1)

--- OOP/minStack.js
+++ OOP/minStack.js
@@ -1,19 +1,29 @@
-class MinStack {
-    constructor(){
-        this.storage = [];
-        this.min = Infinity;
-    }
-}
-
 /** 
  * @param {number} val
  * @return {void}
  */
-MinStack.prototype.push = function(val) {
-    this.storage.push(val);
-    if(this.min > val) {
-        this.min = val;
+
+// Important way to have min value kept at O(1) value, create tuples and the min be in a stack
+
+class MinStack {
+    constructor(){
+        this.storage = []; // data is stored [[val, min], [val,min]];
+        this.min = [];
     }
+}
+
+
+MinStack.prototype.push = function(val) {
+    // if the newest val is smaller than the current min, push it into the min array
+    if(this.min.length === 0) {
+        this.min.push(val);
+    } else if (val < this.min[this.min.length - 1]) {
+        this.min.push(val);    
+    }
+    
+    // if the val is larger than current min, just push val into storage
+    this.storage.push([val, this.min[this.min.length - 1]]);
+   
 };
 
 /**
@@ -21,7 +31,10 @@ MinStack.prototype.push = function(val) {
  */
 MinStack.prototype.pop = function() {
     const data = this.storage.pop();
-    return data;
+    
+    // if the data popped is the same as the current min, pop both of them because that IS the min
+    if(data[0] === this.min[this.min.length - 1]) this.min.pop();
+    return data[0];
 };
 
 /**
@@ -29,27 +42,23 @@ MinStack.prototype.pop = function() {
  */
 MinStack.prototype.top = function() {
     const data = this.storage[this.storage.length - 1];
-  return data;
+    return data[0];
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    return this.min;
+    // with data storage as tuples [val, min] we will always have the latest min
+    const data = this.storage[this.storage.length - 1];
+    return data[1];
 };
 
-// const minStack = new MinStack();
-// minStack.push(5)
-// minStack.push(10)
-// console.log(minStack)
-
-
-  var obj = new MinStack()
-  obj.push(5)
-console.log( obj.pop()) 
-    var param_3 = obj.top()
-var param_4 = obj.getMin()
-
-console.log(param_3)
-console.log(param_4);
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
