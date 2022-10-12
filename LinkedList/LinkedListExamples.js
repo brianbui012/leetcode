af272bdc5dc3bfd93c918234777835980ce7e9ed
Brian Bui <brianbui012@gmail.com>
2022-10-11 18:18:21 -0700

    Updated with zipzag function

--- LinkedList/LinkedListExamples.js
+++ LinkedList/LinkedListExamples.js
@@ -17,6 +17,7 @@ class Node {
         values.push(current.val);
         current = current.next;
       }
+  
       return values;
     }
   
@@ -42,18 +43,82 @@ class Node {
   
       return head.val + this.totalSumR(head.next);
     }
-
+  
     returnIndex(val) {
-        let current = this.head;
-        let index = 0;
-        while(current !== null) {
-          if(current.val === val) return index;
-          index++;
-          current = current.next;
-        }
-    
-        return -1;
+      let current = this.head;
+      let index = 0;
+      while(current !== null) {
+        if(current.val === val) return index;
+        index++;
+        current = current.next;
       }
+  
+      return -1;
+    }
+  
+    returnIndexOf(head = this.head, index) {
+      if(head === null) return null;
+      if(index === 0) return head.val;
+      
+      return returnIndexOf(this.head.next, index - 1);
+    }
+  
+    // if (current.next === null) this.head = current;
+    
+    reverseLinkedList(node = this.head) {
+      let prev = null;
+      let current = node;
+  
+      while(current !== null) {
+        const next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+      }
+      this.head = prev;
+      return this.head;
+    }
+  
+    reverseLinkedListR(head = this.head, prev = null) {
+      if(head === null) return prev;
+  
+      const next = head.next;
+      head.next = prev;
+      return this.reverseLinkedListR(next, head);
+    }
+  
+    zigZag(list1, list2) {
+      let count = 0;
+      let tail = list1;
+      let current1 = list1.head;
+      let current2 = list2.head;
+  
+      
+      while(current1 && current2) {
+        if(count%2 === 0) {
+          let next = current1.next;
+          current1.next = current2;
+          tail = current2;
+          current1 = next;
+        } else {
+          let next = current2.next;
+          current2.next = current1;
+          tail = current1;
+          current2 = next;
+        }
+        count++;
+      }
+  
+      if(current1 === null) {
+        // connect current 2 to tail of current 1
+        tail.next = current1;
+        
+      } else {
+        // connect current 1 to tail of current 2
+        tail.next = current2;
+      }
+      return list1;
+    }
   }
   
   
@@ -66,6 +131,18 @@ class Node {
   nodeB.next = nodeC;
   nodeC.next = nodeD;
   
+  const node1 = new Node(10);
+  const node2 = new Node(20);
+  const node3 = new Node(30);
+  const node4 = new Node(40);
+  
+  node1.next = node2;
+  node2.next = node3;
+  node3.next = node4;
+  
+  const GG = new LinkedList();
+  GG.head = node1;
+  
   const LL = new LinkedList();
   LL.head = nodeA;
   console.log(LL)
@@ -73,7 +150,8 @@ class Node {
   console.log(LL.arrayify());
   console.log(LL.totalSum());
   console.log(LL.totalSumR());
+  
   console.log(LL.returnIndex(7));
-
+  console.log(LL.reverseLinkedList());
   
-  
+  console.log(GG.zigZag(GG, LL));
